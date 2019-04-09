eda70f778e25496cd8c942e5872b6bbc5431ced1
Md Arman Hassan <md-arman.hassan@students.mq.edu.au>
2019-04-09 14:26:05 +1000

    fixed getData9() fuction

--- Scheduler/src/Job.java
+++ Scheduler/src/Job.java
@@ -1,49 +1,40 @@
-import java.util.*;
 public class Job {
 	
-	private String submitTime;
-	private String jobId;
-	private String estimatedRuntime;
-	private String cores;
-	private String memory;
-	private String disk;
+	private static String submitTime;
+	private static String jobId;
+	private static String estimatedRuntime;
+	private static String cores;
+	private static String memory;
+	private static String disk;
 	
 	private boolean complete = false;
 	
-	private String[] data = new String[7];
-	
-	
-	/* public Job(String d) {
-		setData(d);
-	} */
 	
 	//getter
 	
-	public String[] getData() {
-		return data;
-	}
 	
-	public String getSubmitTime(){
+	
+	public static String getSubmitTime(){
 		return submitTime;
 	}
 	
-	public String getJobId(){
+	public static String getJobId(){
 		return jobId;
 	}
 	
-	public String getEstimatedRuntime(){
+	public static String getEstimatedRuntime(){
 		return estimatedRuntime;
 	}
 	
-	public String getCores(){
+	public static String getCores(){
 		return cores;
 	}
 	
-	public String getMemory(){
+	public static String getMemory(){
 		return memory;
 	}
 	
-	public String getDisk(){
+	public static String getDisk(){
 		return disk;
 	}
 	
@@ -52,38 +43,30 @@ public class Job {
 	}
 	
 	//setter
-	public void setData(String a) {
-		this.data[0] = null;
-		this.data[1] = submitTime;
-		this.data[2] = jobId;
-		this.data[3] = estimatedRuntime;
-		this.data[4] = cores;
-		this.data[5] = memory;
-		this.data[6] = disk;
+
+	
+	public void setSubmitTime(String t){
+		this.submitTime = t;
 	}
 	
-	public void setSubmitTime(int t){
-		this.submitTime = Integer.toString(t);
+	public void setJobId(String jID){
+		this.jobId = jID;
 	}
 	
-	public void setJobId(int jID){
-		this.jobId = Integer.toString(jID);
+	public void setEstimatedRuntime(String rt){
+		this.estimatedRuntime = rt;
 	}
 	
-	public void setEstimatedRuntime(int rt){
-		this.estimatedRuntime = Integer.toString(rt);
+	public void setCores(String c){
+		this.cores = c;
 	}
 	
-	public void setCores(int c){
-		this.cores = Integer.toString(c);
+	public void setMemory(String m){
+		this.memory = m;
 	}
 	
-	public void setMemory(int m){
-		this.memory = Integer.toString(m);
-	}
-	
-	public void setDisk(int d){
-		this.disk = Integer.toString(d);
+	public void setDisk(String d){
+		this.disk = d;
 	}
 	
 	public void setComplited(){
@@ -91,12 +74,26 @@ public class Job {
 	}
 		
 	public Job(int submitTime, int jobId, int estimatedRuntime, int cores, int memory, int disk){
-		setSubmitTime(submitTime);
-		setJobId(jobId);
-		setEstimatedRuntime(estimatedRuntime);
-		setCores(cores);
-		setMemory(memory);
-		setDisk(disk);
+		setSubmitTime(Integer.toString(submitTime));
+		setJobId(Integer.toString(jobId));
+		setEstimatedRuntime(Integer.toString(estimatedRuntime));
+		setCores(Integer.toString(cores));
+		setMemory(Integer.toString(memory));
+		setDisk(Integer.toString(disk));
 	}
 	
+	
+	public static String[] getData() {
+		
+		String[] data = new String[7]; //{null, submitTime, jobId, estimatedRuntime, cores, memory, disk};
+		data[0] = null;
+		data[1] = submitTime;
+		data[2] = jobId;
+		data[3] = estimatedRuntime;
+		data[4] = cores;
+		data[5] = memory;
+		data[6] = disk;
+		
+		return data;
+	}
 }
