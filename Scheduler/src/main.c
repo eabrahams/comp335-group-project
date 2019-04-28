e68c8e5700c6be1b7499972ea994087421a4f813
Ourous <Ourous@users.noreply.github.com>
2019-04-28 13:23:40 +1000

    more doc, remove over-qualification of const members

--- Scheduler/src/main.c
+++ Scheduler/src/main.c
@@ -1,5 +1,5 @@
 #include "socket_client.h"
-#include "config_parser.h"
+#include "system_config.h"
 
 int main(int argc, char **argv) {
 	return 0;
--- Scheduler/src/system_config.h
+++ Scheduler/src/system_config.h
@@ -7,29 +7,32 @@ extern "C" {
 #include <stdbool.h>
 #endif
 
-// TODO: make all members of server_type constant
-struct server_info;
-
 typedef struct server_type {
-	char *name;
-	unsigned limit;
-	unsigned bootTime;
+	char *name; // the human-readable name of the server type
+	unsigned limit; // the maximum number of concurrent instances of this type
+	unsigned bootTime; // how long this type of server takes to start up
 	float rate;
-	unsigned cores;
-	unsigned memory;
-	unsigned disk;
+	unsigned cores; // how many CPU cores this server type offers
+	unsigned memory; // how much memory (in MB) this server type offers
+	unsigned disk; // how much disk space (in MB) this server type offers
 } server_type;
 
-typedef enum server_state { SS_OFFLINE, SS_STARTING, SS_IDLE, SS_INACTIVE, SS_UNAVAILABLE } server_state;
+typedef enum server_state {
+	SS_OFFLINE, // server is offline
+	SS_STARTING, // server is starting up, but hasn't finished yet
+	SS_IDLE, // server is running, but not doing anything
+	SS_ACTIVE, // server is running, and currently doing something
+	SS_UNAVAILABLE // server is otherwise unavailable
+} server_state;
 
 typedef struct server_info {
-	const server_type *const type;
-	const int id;
-	server_state state;
+	const server_type *const type; // the type of this server
+	const int id; // the type-unique identifier of this server
+	server_state state; // the current state of this server
 	unsigned avail_time;
-	unsigned avail_cores;
-	unsigned avail_memory;
-	unsigned avail_disk;
+	unsigned avail_cores; // how many CPU cores this server has available
+	unsigned avail_memory; // how much memory this server has available
+	unsigned avail_disk; // how much disk space this server has available
 } server_info;
 
 typedef struct system_config {
@@ -39,14 +42,21 @@ typedef struct system_config {
 	const size_t num_servers;
 } system_config;
 
+/*
+parses an XML file located at `path`, either returning a pointer
+to a valid system_config on success, or returning nullptr and
+logging to stderr on failure.
+*/
 system_config *parse_config(const char *path) noexcept(true);
-void release_config(system_config *config) noexcept(true);
 
-server_info *server_of_type(const system_config *const config, const server_type *const type) noexcept(true);
+// utility to enable a system_config to be freed in one go
+void free_config(system_config *config) noexcept(true);
 
-void reset_server(server_info *const server) noexcept(true);
+// gets the first server of a given type, all other servers of that type follow it in the same memory region
+server_info *server_of_type(const system_config *config, const server_type *type) noexcept(true);
 
-
+// reset the resources availiable on a server to default values
+void reset_server(server_info *server) noexcept(true);
 
 #ifdef __cplusplus
 }
