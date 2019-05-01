586c04a1c297d43a4ac681838c9786e300c9a772
Ellis Abrahams <ellis.abrahams@students.mq.edu.au>
2019-05-01 12:20:08 +1000

    modified allToLargest algorithm to use job_info struct

--- Scheduler/src/all_to_largest.c
+++ Scheduler/src/all_to_largest.c
@@ -1,5 +1,9 @@
-#include "system_config.h"
+#include <stdlib.h>
+#include <string.h>
+
 #include "socket_client.h"
+#include "system_config.h"
+#include "job_info.h"
 
 void all_to_largest(socket_client *client) {
 	system_config *config = parse_config("system.xml");
@@ -22,20 +26,18 @@ void all_to_largest(socket_client *client) {
 		if (strncmp(resp, "NONE", 4) == 0)
 			break;
 
-		job *j = job_from_string(resp);
-
-		size_t len = snprintf(NULL, 0, "%s %d %s %d", "SCHD", j->id, name, 0) + 1;
-		char *schd_str[len];
-		snprintf(schd_str, len, "%s %d %s %d", "SCHD", j->id, name, 0);
-
-		client_send(client, schd_str);
+		job_info j = job_from_string(resp);
 		free(resp);
-		resp = client_receive(client);
-		if (strncmp(resp, "OK", 2) != 0) {
-			fprintf(stderr, "%s%s\n", "Expected 'OK', but got: ", resp);
+
+		size_t len = snprintf(NULL, 0, "%s %d %s %d", "SCHD", j.id, name, 0) + 1;
+		char *schd_str[len];
+		snprintf(schd_str, len, "%s %d %s %d", "SCHD", j.id, name, 0);
+
+		bool scheduled = client_msg_resp(client, schd_str, "OK");
+		if (!scheduled) {
+			fprintf("%s%d%s\n", "Unable to schedule job ", j.id, " with command: ", schd_str);
 			exit(1);
 		}
-		free(resp);
 	}
 
 	client_send("QUIT");
