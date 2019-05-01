e1ea92885769fdf174063d17a296118f1506a514
Ellis Abrahams <ellis.abrahams@students.mq.edu.au>
2019-05-01 12:34:39 +1000

    updated main.c to use all_to_largest function

--- Scheduler/src/main.c
+++ Scheduler/src/main.c
@@ -1,9 +1,9 @@
 #include "socket_client.h"
-#include "system_config.h"
+#include "all_to_largest.h"
 
 int main(int argc, char **argv) {
 	socket_client *client = client_init(LOCALHOST, DEFAULT_PORT);
-	allToLargest(client);
+	all_to_largest(client);
 	// update_server(server_of_type(config, type) + id, state, time, cores, memory, disk);
 	return 0;
 }
