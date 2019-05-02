f1c7d4c3ebdc71687f9d12823fb09e8cfc75a8b7
Ellis Abrahams <ellis.abrahams@students.mq.edu.au>
2019-05-02 11:02:59 +1000

    moved best_first and all_to_largest into algorithms file

--- Scheduler/src/algorithms.c
+++ Scheduler/src/algorithms.c
@@ -50,3 +50,26 @@ void all_to_largest(socket_client *client) {
 	free_config(config);
 }
 
+void best_first(socket_client *client) {
+	unsigned int best_fit, min_avail;
+	best_fit = min_avail = UINT_MAX;
+	
+	system_config *config = parse_config("system.xml");
+	while (true) {
+		client_send(client, "REDY");
+		char *resp = client_receive(client);
+		if (strncmp(resp, "NONE", 4) == 0)
+			break;
+
+		job_info j = job_from_string(resp);
+		free(resp);
+		int fitness;
+		unsigned int i;
+		for (i = 0; i < config->num_servers; i++) {
+			// find best server for job
+		}
+	}
+
+	free_config(config);
+}
+
---
+++ Scheduler/src/algorithms.h
@@ -0,0 +1,9 @@
+#ifndef algorithms_h_
+#define algorithms_h_
+
+#include "socket_client.h"
+
+void all_to_largest(socket_client *client);
+void best_first(socket_client *client);
+
+#endif
