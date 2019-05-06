3a1a6211f61ac7b07bebb26a855da3c7c2946b01
Ourous <gs@ourous.net>
2019-05-06 12:54:59 +1000

    add server RESC command handling

--- Scheduler/src/system_config.h
+++ Scheduler/src/system_config.h
@@ -5,12 +5,14 @@
 #include "resource_info.h"
 
 #ifdef __cplusplus
-#include <cstddef>
+#include "cpp_util.h"
 extern "C" {
 #else
 #define noexcept
 #include <stdbool.h>
 #endif
+
+#include "socket_client.h"
 
 typedef struct server_type {
 	char *name; // the human-readable name of the server type
@@ -18,6 +20,9 @@ typedef struct server_type {
 	unsigned bootTime; // how long this type of server takes to start up
 	float rate;
 	resource_info max_resc; // the resources offered by this server type
+#ifdef __cplusplus
+	void release() noexcept;
+#endif
 } server_type;
 
 typedef enum server_state {
@@ -32,19 +37,37 @@ typedef struct server_info {
 	const server_type *type; // the type of this server
 	int id; // the type-unique identifier of this server
 	server_state state; // the current state of this server
-	unsigned avail_time;
+	int avail_time; // can be -1, meaning "available now" (based on RESC example info from spec)
 	resource_info avail_resc; // the available resources on this server
 #ifdef __cplusplus
-	bool update(const server_state &state, const unsigned &time, const resource_info &resc) noexcept;
+	bool update(server_state state, int time, const resource_info &resc) noexcept;
 	void reset() noexcept;
 #endif
 } server_info;
+
+// container for an arbitrary collection of servers
+typedef struct server_group {
+	server_info *const * servers; // flat collection, no order
+	size_t num_servers; // number of servers
+#ifdef __cplusplus
+	void release() noexcept;
+#endif
+} server_group;
 
 typedef struct system_config {
 	const server_type *types; // collection of types, ordered as parsed from XML
 	size_t num_types; // number of types
 	server_info *servers; // flat collection of servers, ordered by type then id
 	size_t num_servers; // number of servers
+#ifdef __cplusplus
+	// handler for `RESC All`
+	void update(socket_client *client);
+	// handler for `RESC Type ..`
+	void update(socket_client *client, const server_type *type);
+	// handler for `RESC Avail ..`, returns the servers updated
+	std::vector<server_info*> update(socket_client *client, const resource_info &resc);
+	void release() noexcept;
+#endif
 } system_config;
 
 /*
@@ -57,14 +80,23 @@ system_config *parse_config(const char *path) noexcept;
 // utility to enable a system_config to be freed in one go
 void free_config(system_config *config) noexcept;
 
+// utility to enable a server_group to be freed in one go
+void free_group(server_group *group) noexcept;
+
 // gets the server_type matching a given name
 const server_type *type_by_name(const system_config *config, const char* name) noexcept;
 
 // gets the first server of a given type, all other servers of that type follow it in the same memory region
-server_info *servers_by_type(const system_config *config, const server_type *type) noexcept;
+server_info *start_of_type(const system_config *config, const server_type *type) noexcept;
+
+bool update_config(system_config *config, socket_client *client) noexcept;
+
+bool update_servers_by_type(system_config *config, socket_client *client, const server_type *type) noexcept;
+
+server_group *updated_servers_by_avail(system_config *config, socket_client *client, const resource_info resc) noexcept;
 
 // validates the new resources for the server, returning true and updating if they're valid; false otherwise
-bool update_server(server_info *server, server_state state, unsigned time, resource_info resc) noexcept;
+//bool update_server(server_info *server, server_state state, int time, resource_info resc) noexcept;
 
 // reset the resources availiable on a server to default values
 void reset_server(server_info *server) noexcept;
