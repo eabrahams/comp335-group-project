fc1d0447fc646af8c13dd9d414a0f5821189f701
Ourous <Ourous@users.noreply.github.com>
2019-05-08 20:22:50 +1000

    finish up job_info test suite

--- Scheduler/tst/job_info.test.cpp
+++ Scheduler/tst/job_info.test.cpp
@@ -12,30 +12,30 @@ namespace {
 	}
 
 	TEST(JobFromString, AllZeroes) {
-		constexpr job_info ref_job = job_info{ 0, 0, 0, resource_info{0, 0, 0} };
-		job_info str_job = job_from_string("JOBN 0 0 0 0 0 0");
-		EXPECT_EQ(str_job.submit_time, ref_job.submit_time);
-		EXPECT_EQ(str_job.id, ref_job.id);
-		EXPECT_EQ(str_job.est_runtime, ref_job.est_runtime);
-		EXPECT_EQ(str_job.req_resc, ref_job.req_resc);
+		constexpr job_info expected = job_info{ 0, 0, 0, resource_info{0, 0, 0} };
+		job_info job = job_from_string("JOBN 0 0 0 0 0 0");
+		EXPECT_EQ(job.submit_time, expected.submit_time);
+		EXPECT_EQ(job.id, expected.id);
+		EXPECT_EQ(job.est_runtime, expected.est_runtime);
+		EXPECT_EQ(job.req_resc, expected.req_resc);
 	}
-	
+
 	TEST(JobFromString, StandardExample0) {
-		constexpr job_info ref_job = job_info{ 83, 0, 1566, resource_info{1, 200, 300} };
-		job_info str_job = job_from_string("JOBN 83 0 1566 1 200 300");
-		EXPECT_EQ(str_job.submit_time, ref_job.submit_time);
-		EXPECT_EQ(str_job.id, ref_job.id);
-		EXPECT_EQ(str_job.est_runtime, ref_job.est_runtime);
-		EXPECT_EQ(str_job.req_resc, ref_job.req_resc);
+		constexpr job_info expected = job_info{ 83, 0, 1566, resource_info{1, 200, 300} };
+		job_info job = job_from_string("JOBN 83 0 1566 1 200 300");
+		EXPECT_EQ(job.submit_time, expected.submit_time);
+		EXPECT_EQ(job.id, expected.id);
+		EXPECT_EQ(job.est_runtime, expected.est_runtime);
+		EXPECT_EQ(job.req_resc, expected.req_resc);
 	}
 
 	TEST(JobFromString, StandardExample1) {
-		constexpr job_info ref_job = job_info{ 290, 1, 7, resource_info{1, 400, 1200} };
-		job_info str_job = job_from_string("JOBN 290 1 7 1 400 1200");
-		EXPECT_EQ(str_job.submit_time, ref_job.submit_time);
-		EXPECT_EQ(str_job.id, ref_job.id);
-		EXPECT_EQ(str_job.est_runtime, ref_job.est_runtime);
-		EXPECT_EQ(str_job.req_resc, ref_job.req_resc);
+		constexpr job_info expected = job_info{ 290, 1, 7, resource_info{1, 400, 1200} };
+		job_info job = job_from_string("JOBN 290 1 7 1 400 1200");
+		EXPECT_EQ(job.submit_time, expected.submit_time);
+		EXPECT_EQ(job.id, expected.id);
+		EXPECT_EQ(job.est_runtime, expected.est_runtime);
+		EXPECT_EQ(job.req_resc, expected.req_resc);
 	}
 
 	TEST(JobFromString, StandardExample2) {
@@ -108,5 +108,59 @@ namespace {
 		EXPECT_EQ(str_job.id, ref_job.id);
 		EXPECT_EQ(str_job.est_runtime, ref_job.est_runtime);
 		EXPECT_EQ(str_job.req_resc, ref_job.req_resc);
+	}
+
+	TEST(JobCanRun, ExcessiveResources) {
+		constexpr job_info job = job_info{ 0, 0, 0, resource_info{1, 2, 3} };
+		constexpr auto resources = resource_info{ 4, 4, 4 };
+		EXPECT_TRUE(job.can_run(resources));
+	}
+
+	TEST(JobCanRun, AdequateResources) {
+		constexpr job_info job = job_info{ 0, 0, 0, resource_info{1, 2, 3} };
+		constexpr auto resources = resource_info{ 3, 3, 3 };
+		EXPECT_TRUE(job.can_run(resources));
+	}
+
+	TEST(JobCanRun, ExactResources) {
+		constexpr job_info job = job_info{ 0, 0, 0, resource_info{1, 2, 3} };
+		constexpr auto resources = resource_info{ 1, 2, 3 };
+		EXPECT_TRUE(job.can_run(resources));
+	}
+
+	TEST(JobCanRun, InadequateResources) {
+		constexpr job_info job = job_info{ 0, 0, 0, resource_info{1, 2, 3} };
+		constexpr auto resources = resource_info{ 0, 1, 2 };
+		EXPECT_FALSE(job.can_run(resources));
+	}
+
+	TEST(JobCanRun, ZeroResources) {
+		constexpr job_info job = job_info{ 0, 0, 0, resource_info{1, 2, 3} };
+		constexpr auto resources = resource_info{ 0, 0, 0 };
+		EXPECT_FALSE(job.can_run(resources));
+	}
+
+	TEST(JobCanRun, MismatchResources) {
+		constexpr job_info job = job_info{ 0, 0, 0, resource_info{1, 2, 3} };
+		constexpr auto resources = resource_info{ 3, 2, 1 };
+		EXPECT_FALSE(job.can_run(resources));
+	}
+
+	TEST(JobFitness, PositiveFitness) {
+		constexpr auto job = job_info{ 0, 0, 0, resource_info {99, 0, 0} };
+		constexpr auto resc = resource_info{ 100, 0, 0 };
+		EXPECT_EQ(job.fitness(resc), 1);
+	}
+
+	TEST(JobFitness, ZeroFitness) {
+		constexpr auto job = job_info{ 0, 0, 0, resource_info {100, 0, 0} };
+		constexpr auto resc = resource_info{ 100, 0, 0 };
+		EXPECT_EQ(job.fitness(resc), 0);
+	}
+
+	TEST(JobFitness, NegativeFitness) {
+		constexpr auto job = job_info{ 0, 0, 0, resource_info{100, 0, 0} };
+		constexpr auto resc = resource_info{ 99, 0, 0 };
+		EXPECT_EQ(job.fitness(resc), -1);
 	}
 }
