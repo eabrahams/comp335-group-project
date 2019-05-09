6dee245e62b85845110e4102b9b6ee98a69ef6eb
Ellis Abrahams <ellis.abrahams@students.mq.edu.au>
2019-05-10 09:36:15 +1000

    don't connect until arguments have been sorted out

--- Scheduler/src/main.c
+++ Scheduler/src/main.c
@@ -8,8 +8,6 @@
 void usage(char *name);
 
 int main(int argc, char **argv) {
-	socket_client *client = client_init(LOCALHOST, DEFAULT_PORT);
-
 	void *algorithm = &all_to_largest;
 
 	int i;
@@ -34,6 +32,8 @@ int main(int argc, char **argv) {
 			usage(argv[0]);
 		}
 	}
+
+	socket_client *client = client_init(LOCALHOST, DEFAULT_PORT);
 
 	run_algorithm(client, algorithm);
 
