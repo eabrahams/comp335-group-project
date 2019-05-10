bbba50441e12298c76692940ae118843097f2eba
Ourous <Ourous@users.noreply.github.com>
2019-05-10 15:44:47 +1000

    const-correctness, unit tests

--- Scheduler/src/stringhelper.h
+++ Scheduler/src/stringhelper.h
@@ -5,13 +5,15 @@
 #include <pcre2.h>
 #include "job_info.h"
 
+#define JOB_REGEX "JOBN ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)"
+
 typedef struct {
 	pcre2_code *re;
 	pcre2_match_data *match_data;
 } regex_info;
 
-regex_info *regex_init(char *pattern);
-job_info strtojob(char *jobstr, regex_info *regex);
+regex_info *regex_init(const char *pattern);
+job_info strtojob(const char *jobstr, regex_info *regex);
 char *create_schd_str(unsigned int id, char *server_name, int server_id);
 
 #endif
