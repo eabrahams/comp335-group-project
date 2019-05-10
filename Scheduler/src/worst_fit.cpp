a1e142d07ba7eac0ff5d6a9e53acb82a5d278d3a
Ourous <Ourous@users.noreply.github.com>
2019-05-10 15:31:30 +1000

    fix algorithm

--- Scheduler/src/worst_fit.cpp
+++ Scheduler/src/worst_fit.cpp
@@ -8,12 +8,16 @@
 #include <limits>
 #include <sstream>
 
-constexpr unsigned WORST_FIT_AVAIL_TIME_THRESHOLD = 600; // ten minutes
+constexpr int WORST_FIT_AVAIL_TIME_THRESHOLD = 6000; // ten minutes
 
 server_info *worst_fit(system_config* config, server_group *candidates, job_info job) {
 	int worst_fit, other_fit, type_fit;
 	server_info *worst_server, *other_server, *type_server;
-	worst_fit = other_fit = type_fit = std::numeric_limits<int>::min();
+	worst_fit = std::numeric_limits<int>::min();
+	other_fit = std::numeric_limits<int>::min();
+	type_fit = std::numeric_limits<int>::min();
+	int other_avail_time = WORST_FIT_AVAIL_TIME_THRESHOLD;// job.est_runtime;
+	server_state type_state = server_state::SS_INACTIVE;
 
 	for(auto s = 0; s < candidates->num_servers; ++s) {
 		auto *server = candidates->servers[s];
@@ -21,9 +25,10 @@ server_info *worst_fit(system_config* config, server_group *candidates, job_info
 		if(fitness > worst_fit && server->state == server_state::SS_IDLE) {
 			worst_fit = fitness;
 			worst_server = server;
-		} else if(fitness >= other_fit && server->avail_time <= WORST_FIT_AVAIL_TIME_THRESHOLD) {
+		} else if(fitness > other_fit && server->avail_time <= other_avail_time) {
 			other_fit = fitness;
 			other_server = server;
+			other_avail_time = server->avail_time;
 		}
 	}
 
@@ -36,14 +41,20 @@ server_info *worst_fit(system_config* config, server_group *candidates, job_info
 		server_info *server_offset = start_of_type(config, type);
 		for(auto s = 0; s < type->limit; ++s) {
 			auto server = &server_offset[s];
-			if(server->state != server_state::SS_ACTIVE) continue;
+			if(server->state == server_state::SS_UNAVAILABLE) continue;
+			//if(server->state != server_state::SS_ACTIVE) continue;
 			int fitness = job.fitness(server->avail_resc);
-			if(fitness >= type_fit) {
+			if(server->state > type_state) {
 				type_fit = fitness;
 				type_server = server;
+				type_state = server->state;
+			} else if(fitness > type_fit && server->state >= type_state) {
+				type_fit = fitness;
+				type_server = server;
+				//type_state = server->state;
 			}
 		}
 	}
-	
+
 	return type_server;
 }
